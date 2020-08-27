56674576a35865fb602aefe1e17cc0b0b9d76690
rasools <rasools@chalmers.se>
2020-08-27 14:01:01 +0200

    first upload

--- scripts/35samples_DEA_deseq2.R
+++ scripts/35samples_DEA_deseq2.R
@@ -1,31 +1,44 @@
+# Differential expression analysis by deseq2
+# 
+# 
+# Rasool Saghaleyni   2020-08-25
+####################################################################
 
+# *** CHANGE THIS PATH TO REPOSITORY PATH IN YOUR COMPUTER ***
+pathToEpoGfp <- '/path/to/epo_gfp_repo/'
+
+# add needed libraries for doing analysis in this analysis
 library(DESeq2)
+library(biomaRt)
+library(ggplot2)
+library(ReportingTools)
+library(regionReport)
+library(pheatmap)
+library(RColorBrewer)
+library(ggplot2)
+
 # import counts
-counts <- read.delim("/Users/rasools/Box Sync/phd/ppf/293F_PSN/alignment/tximport_outputs/35samples_counts.txt")
+counts <- read.delim(paste(pathToEpoGfp,'data/35samples_counts.txt',sep=''))
 rownames(counts) <- counts$Geneid
 counts <- counts[,-1]
 counts <- ceiling(counts)
 colnames(counts) <- gsub("counts.","", colnames(counts))
-#counts$EPOF21_I_6 <- NULL
 counts <- counts[c(2,1,4,3,6,5,8,7,10,9,13,11,12,15,14,17,16,19,18,21,20,24,25,22,23,27,26,29,28,31,30,33,32,35,34)]
 # import TPMs
-TPM <- read.delim("/Users/rasools/Box Sync/phd/ppf/293F_PSN/alignment/tximport_outputs/35samples_TPM.txt")
+TPM <- read.delim(paste(pathToEpoGfp,'data/35samples_TPM.txt',sep=''))
 rownames(TPM) <- TPM$Geneid
 TPM <- TPM[,-1]
 colnames(TPM) <- gsub("abundance.","", colnames(TPM))
-#TPM$EPOF21_I_6 <- NULL
 TPM <- TPM[c(2,1,4,3,6,5,8,7,10,9,13,11,12,15,14,17,16,19,18,21,20,24,25,22,23,27,26,29,28,31,30,33,32,35,34)]
 # check
 all(colnames(TPM) == colnames(counts))
 # import samples table
-kallisto.dir <- list.files(path = "/Users/rasools/Box\ Sync/phd/ppf/293F_PSN/alignment/kallisto_outputs")
+kallisto.dir <- list.files(path = paste(pathToEpoGfp,'data/kallisto_outputs/',sep=''))
 nam <- substring(sapply(strsplit(kallisto.dir, "_lib"), "[", 1),10)
 samples <- data.frame(replicate = c(gsub("lib.+_.+_","",substring(kallisto.dir,10))), sampleName = c(sapply(strsplit(nam, "_"),"[",1)), protein = rep(c("GFP_Control","EPO_Control","EPO","GFP"), c(2,2,13,18)), producibility = rep(c("Control","Producer"), c(4,31)), cellType = rep(c("HEK293F","HEK293freestyle","HEK293F"), c(2,15,18)), growth = rep(c(0.0254,0.0257,0.0227,0.02,0.0241,0.024,0.0223,0.0257,0.0254,0.0246,0.0218,0.0226,0.0228,0.0254,0.0243,0.0226), c(2,2,2,2,2,3,2,2,2,2,4,2,2,2,2,2)), epoQP = rep(c(NaN,0,3.67,4.05,3.35,13.9,2.87,2.35,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN), c(2,2,2,2,2,3,2,2,2,2,4,2,2,2,2,2)), gfpQP = rep(c(0,NaN,NaN,NaN,NaN,NaN,NaN,NaN,4.31,3.86,4.29,2.27,3.15,2.48,1.16,1), c(2,2,2,2,2,3,2,2,2,2,4,2,2,2,2,2)))
 rownames(samples) <- samples[,1]
+# check
 all(rownames(samples) == colnames(counts))
-
-# remove line related to EPO_F_21_6
-# samples <- samples[-c(11),]
 # add epoTPM column to samples table
 samples$epoL2TPM <- log(as.vector(t(TPM["EPO_HPC4_splitGFP",])),2)
 samples$epoL2TPM[which(!is.finite(samples$epoL2TPM))] <- 0
@@ -33,7 +46,6 @@ samples$epoL2TPM[which(!is.finite(samples$epoL2TPM))] <- 0
 #---------------------------------------------------------------
 # #for comparing GFP-producers with HEK293F control cell-line
  samples <- samples[samples$cellType == "HEK293F",]
-# samples$gfpTPM <- t(counts["pD2529_GFP_extra",])
  counts <- counts[, rownames(samples)]
 #---------------------------------------------------------------
 # for comparing EPO-producers with f-free control cell-line
@@ -105,7 +117,6 @@ sig <- as.data.frame(sig)
 a <- as.data.frame(row.names(sig))
 rownames(a) <- a$`row.names(sig)`
 #---------------------------------------------------------------
-library(biomaRt)
 ensembl  <- useMart(host="www.ensembl.org", "ENSEMBL_MART_ENSEMBL",  dataset="hsapiens_gene_ensembl")
 genemap <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
                  filters = "ensembl_gene_id",
@@ -124,12 +135,10 @@ write.table(sig, "/Users/rasools/Desktop/f21_pairwise/F21_vs_EPOproducers_excl_E
 #---------------------------------------------------------------
 d <- plotCounts(ds, gene="EPO_HPC4_splitGFP", intgroup="protein", 
                 returnData=TRUE)
-library("ggplot2")
 ggplot(d, aes(x=protein, y=count)) + 
   geom_point(position=position_jitter(w=0.1,h=0))  
   #+ scale_y_log10(breaks=c(25,100,400))
 #---------------------------------------------------------------ReportingTools
-library("ReportingTools")
 desReport <- HTMLReport(shortName = 'EPO_producers',
     title = 'EPO_producers',
     reportDirectory = "./reports")
@@ -138,10 +147,8 @@ publish(ds,desReport, pvalueCutoff=0.1,
 	reportDir="./reports")
 finish(desReport)
 #---------------------------------------------------------------regionReport
-library("regionReport")
 report <- DESeq2Report(ds, project = "EPO_producers", c('sampleName','protein'), outdir = "DESeq2Report-example")
 #---------------------------------------------------------------Heatmap of the count matrix
-library("pheatmap")
 ntd <- normTransform(ds)
 select <- order(rowMeans(counts(ds,normalized=TRUE)),
                 decreasing=TRUE)[1:100]
@@ -164,7 +171,6 @@ pheatmap(assay(rld)[select,], cluster_rows=FALSE, show_rownames=TRUE,
          cluster_cols=TRUE, annotation_col=df)
 #---------------------------------------------------------------Heatmap of the sample-to-sample distances
 sampleDists <- dist(t(assay(vsd)))
-library("RColorBrewer")
 sampleDistMatrix <- as.matrix(sampleDists)
 rownames(sampleDistMatrix) <- paste(vsd$sampleName, vsd$protein, sep="-")
 colnames(sampleDistMatrix) <- NULL
@@ -174,7 +180,6 @@ pheatmap(sampleDistMatrix,
          clustering_distance_cols=sampleDists,
          col=colors)
 #---------------------------------------------------------------Principal component plot of the samples
-library("ggplot2")
 vsd <- vst(ds, blind=FALSE)
 rld <- rlog(ds, blind=FALSE)
 pcaData <- plotPCA(rld, intgroup=c("sampleName", "protein"), returnData=TRUE)
